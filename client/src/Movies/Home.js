46b5103743f3216b8d26ff502f5c5f9919707fa0
Tate Keller <tatek1993@gmail.com>
2020-01-28 16:50:42 -0600

    created Home.js and imported it to savedList, created routeToHome fn to add onClick functionallity to home button

--- client/src/Movies/Home.js
+++ client/src/Movies/Home.js
@@ -4,12 +4,11 @@ import { useHistory } from "react-router-dom";
 function Home() {
     const history = useHistory();
     console.log("This is our history", history);
-    const routeToMovieList = () => {
-        setTimeout(() => {
-            
-        })
+    const routeToHome = () => {
+        history.push("/");
     }
     return (
-        
-    )
+        <div onClick={routeToHome} className="home-button">Home</div>
+    );
 }
+export default Home;
